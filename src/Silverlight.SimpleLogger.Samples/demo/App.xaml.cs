51a74ae4cf344b4f4ec64f996fa6e6f34a032973
Oleg Volkov <OGVolkov@gmail.com>
2011-10-27 10:13:40 +0300

    Added small sample app

---
+++ src/Silverlight.SimpleLogger.Samples/demo/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows;
+
+using Silverlight.SimpleLogger;
+
+namespace demo
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            this.Startup += this.Application_Startup;
+            this.Exit += this.Application_Exit;
+            this.UnhandledException += this.Application_UnhandledException;
+
+            InitializeComponent();
+
+            // configure logger
+            SilverlightLogger.Configure(SilverlightLogLevel.All, "SilverlightApp.log");
+        }
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            this.RootVisual = new MainPage();
+        }
+
+        private void Application_Exit(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Application_UnhandledException(object sender, ApplicationUnhandledExceptionEventArgs e)
+        {
+            // log the error
+            SilverlightLogger.Error("Error happened", e.ExceptionObject);
+        }        
+    }
+}
---
+++ src/Silverlight.SimpleLogger.Samples/demo/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+using Silverlight.SimpleLogger;
+
+namespace demo
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void GoodButton_Click(object sender, RoutedEventArgs e)
+        {            
+            SilverlightLogger.Info("Entering good button click with random value={0}", new Random().Next());
+        }
+
+        private void BadButton_Click(object sender, RoutedEventArgs e)
+        {
+            // simulate error
+            var x = 0;
+            x = x / x;
+        }
+    }
+}
